f0040b098cbf1acc892095cf64cfc39b3ac0d3b4
Cptzeph23 <asekazephaniah>
2025-11-11 02:34:55 +0300

    Initial Commit

---
+++ bridgeapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def portfolio(request):
+    return render(request, 'portfolio-details.html')
+
+def services(request):
+    return render(request, 'service-details.html')
+
+def starter(request):
+    return render(request, 'starter-page.html')
+
